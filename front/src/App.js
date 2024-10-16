b80af4c55b6aad0a184efcf965cd711097df3af9
LuisYou <loisikr@naver.com>
2024-10-16 15:00:14 +0900

    테스트 페이지

---
+++ front/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>메인 페이지 테스트 중...</h1>
+        <hr></hr>
+        <button>버튼 1</button>
+        <button>버튼 2</button>
+        <button>버튼 3</button>
+      </div>
+    );
+  }
+}
+
+export default App;
