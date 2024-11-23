480012eb91f3984d341440672f6dab3823c5f904
LuisYou <loisikr@naver.com>
2024-11-23 20:18:30 +0900

    병합 취소

--- front/src/MainPage.js
+++ front/src/MainPage.js
@@ -12,6 +12,15 @@ import { Dialog } from "@mui/material";
 import config from './config';
 import Card from "@mui/material"
 
+const Card = styled(MuiCard)(({ theme }) => ({
+  boxShadow:
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  ...theme.applyStyles('dark', {
+    boxShadow:
+      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
+}));
+
 class MainPage extends Component {
     render() {
       return (
